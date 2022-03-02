d1db8fa9af0a4d9e68a73ca92152d13b2764633f
Ayushi <ayushi.khandelwal@credable.in>
2022-03-02 11:28:06 +0530

    adding diver manager

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,50 @@
+//package org.example;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.Keys;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        // only for me
+////        ChromeOptions options = new ChromeOptions();
+////        options.setBinary("/usr/bin/brave-browser");
+//        WebDriverManager.chromedriver().setup();
+//
+//        WebDriver driver = new ChromeDriver();
+////        FirefoxDriver driver=new FirefoxDriver();
+//
+//        driver.get("http://google.com");
+//
+//        // document.getElementsByName("q")[0]
+//        WebElement searchBox = driver.findElement(By.name("q"));
+//        searchBox.click();
+//        // document.getElementsByName("q")[0].value = "Selenium Testing"
+//        searchBox.sendKeys("Selenium Testing");
+//        searchBox.sendKeys(Keys.ENTER);
+//        driver.findElement(By.xpath("//a[@href='https://www.guru99.com/introduction-to-selenium.html']")).click();
+//    }
+//}
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
-    public static void main(String[] args) {
-        // only for me
-//        ChromeOptions options = new ChromeOptions();
-//        options.setBinary("/usr/bin/brave-browser");
 
-        WebDriver driver = new ChromeDriver();
-//        FirefoxDriver driver=new FirefoxDriver();
+    public static void main(String[] args) {
+
+        WebDriver driver = chromeDriver();
 
         driver.get("http://google.com");
 
@@ -24,6 +54,19 @@ public class Main {
         // document.getElementsByName("q")[0].value = "Selenium Testing"
         searchBox.sendKeys("Selenium Testing");
         searchBox.sendKeys(Keys.ENTER);
-        driver.findElement(By.xpath("//a[@href='https://www.guru99.com/introduction-to-selenium.html']")).click();
     }
+
+    public static WebDriver firefoxDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
+    }
+
+    public static WebDriver chromeDriver() {
+//        ChromeOptions options = new ChromeOptions();
+//        options.setBinary("/usr/bin/brave-browser");
+
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+
 }
