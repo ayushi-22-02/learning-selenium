5e238164e8ca5dea349f0fbb98f635100c2a030d
Ayushi <ayushi.khandelwal@credable.in>
2022-03-04 10:26:03 +0530

    opening the sekenium dev and adding dependency for junit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,34 +1,3 @@
-//package org.example;
-//
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.Keys;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        // only for me
-////        ChromeOptions options = new ChromeOptions();
-////        options.setBinary("/usr/bin/brave-browser");
-//        WebDriverManager.chromedriver().setup();
-//
-//        WebDriver driver = new ChromeDriver();
-////        FirefoxDriver driver=new FirefoxDriver();
-//
-//        driver.get("http://google.com");
-//
-//        // document.getElementsByName("q")[0]
-//        WebElement searchBox = driver.findElement(By.name("q"));
-//        searchBox.click();
-//        // document.getElementsByName("q")[0].value = "Selenium Testing"
-//        searchBox.sendKeys("Selenium Testing");
-//        searchBox.sendKeys(Keys.ENTER);
-//        driver.findElement(By.xpath("//a[@href='https://www.guru99.com/introduction-to-selenium.html']")).click();
-//    }
-//}
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -44,8 +13,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        WebDriver driver = chromeDriver();
+        WebDriver driver = firefoxDriver();
 
+        // testGoogle(driver);
+        testSeleniumDev(driver);
+    }
+
+    private static void testGoogle(WebDriver driver) {
         driver.get("http://google.com");
 
         // document.getElementsByName("q")[0]
@@ -56,16 +30,28 @@ public class Main {
         searchBox.sendKeys(Keys.ENTER);
     }
 
+    public static void testSeleniumDev(WebDriver driver) {
+        driver.get("https://selenium.dev");
+        WebElement blog = driver.findElement(By.linkText("Blog"));
+        blog.click();
+
+        WebElement link2016 = driver.findElement(By.id("m-blog2016"));
+        link2016.click();
+
+        WebElement element = driver.findElement(By.linkText("Fall Selenium Conf, Save the Date & Call for Speakers!"));
+        element.click();
+    }
+
+
     public static WebDriver firefoxDriver() {
         WebDriverManager.firefoxdriver().setup();
         return new FirefoxDriver();
     }
 
     public static WebDriver chromeDriver() {
+        WebDriverManager.chromedriver().setup();
 //        ChromeOptions options = new ChromeOptions();
 //        options.setBinary("/usr/bin/brave-browser");
-
-        WebDriverManager.chromedriver().setup();
         return new ChromeDriver();
     }
 
