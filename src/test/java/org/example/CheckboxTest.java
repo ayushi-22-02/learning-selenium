c2dc4d629bb81295981184081d0e793c2063e6bc
Ayushi <ayushi.khandelwal@credable.in>
2022-03-04 13:57:18 +0530

    checking for loading message

--- src/test/java/org/example/CheckboxTest.java
+++ src/test/java/org/example/CheckboxTest.java
@@ -11,8 +11,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.awt.*;
+import java.time.Duration;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CheckboxTest {
     private WebDriver driver;
@@ -49,8 +55,21 @@ public class CheckboxTest {
           System.out.println(mayo.isSelected());
           System.out.println(turkey.isSelected());
 
-        assertEquals(true,lettuce.isSelected());
+
+        assertTrue(lettuce.isSelected());
 
     }
 
+
+
+    @Test
+    public void test_loading_complete_message() {
+        WebElement checkbox1 = driver.findElement(By.id("checkboxLink"));
+        checkbox1.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("message")));
+
+        WebElement msg = driver.findElement(By.id("message"));
+        assertEquals("Loading complete...", msg.getText());
+    }
 }
